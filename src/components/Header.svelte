9c520e80e4cea0a4eb16e4625831daf69cbd8582
benwizschool <ben@wizschool.io>
2021-08-28 13:21:00 +0900

    Add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<header>
+  <div>
+    <h1>🚀 Rocket Fun Facts</h1>
+  </div>
+</header>
+
+<style>
+  header {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    margin: 0 auto 3rem;
+    max-width: min(100%, 68ch);
+  }
+</style>
